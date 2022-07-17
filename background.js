b8eb0516a624069f1f727184729eb908da9fa1dd
ワイくん <105576113+himazin355@users.noreply.github.com>
2022-07-18 05:00:32 +0900

    変数宣言をletに変更

--- background.js
+++ background.js
@@ -25,18 +25,15 @@ function createContextMenu() {
  * @param object info
  */
 function copyByContextMenu(info) {
-    var mediaType = info.mediaType;
-    var srcUrl = info.srcUrl;
-    var linkUrl = info.linkUrl;
-    var selectionText = info.selectionText;
-    var isSelectedText = typeof selectionText !== 'undefined';
-
-    var ta = document.createElement('textarea');
-
+    let mediaType = info.mediaType;
+    let srcUrl = info.srcUrl;
+    let linkUrl = info.linkUrl;
+    let selectionText = info.selectionText;
+    let isSelectedText = typeof selectionText !== 'undefined';
+    let ta = document.createElement('textarea');
     // コンテキストメニューを出した場所が画像の場合、画像を表示するMD形式の文字列を作成
     if (mediaType === 'image') {
         ta.value = '![altテキスト](' + srcUrl + ')';
-
     // コンテキストメニューを出した場所がリンクの場合、リンクを表示するMD形式の文字列を作成
     } else if (typeof linkUrl !== 'undefined') {
         if (isSelectedText) {
@@ -45,7 +42,6 @@ function copyByContextMenu(info) {
             ta.value = '[' + linkUrl + '](' + linkUrl + ')';
         }
     }
-
     document.body.appendChild(ta);
     ta.select();
     document.execCommand('copy');
@@ -58,10 +54,8 @@ function copyByContextMenu(info) {
  * @param object info
  */
 function copyByBrowserAction(tab) {
-    var ta = document.createElement('textarea');
-
+    let ta = document.createElement('textarea');
     ta.value = '[' + tab.title + '](' + tab.url + ')';
-
     document.body.appendChild(ta);
     ta.select();
     document.execCommand('copy');
